5ecfb23fd3fde02a459bb568756f516d972836cc
gmioannou <gmioannou@gmail.com>
2019-06-25 21:28:57 +0300

    Migrate to QGIS 3

--- core/pgdb.py
+++ core/pgdb.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 /***************************************************************************
- DataManager
+ DomainManager
 								 A QGIS plugin
  This plugin manages Layers, Tables, Joins and Relates in PostgreSQL/PostGIS database
 							  -------------------
@@ -25,86 +25,91 @@ import psycopg2
 from qgis.core import QgsMessageLog
 
 class Pgdb:
+    def __init__(self):
+        """Constructor."""
+        self.database = ''
+        self.host = ''
+        self.port = ''
+        self.username = ''
+        self.password = ''
 
-	def __init__(self):
-		"""Constructor."""
-		self.database = ''
-		self.host = ''
-		self.port = ''
-		self.username = ''
-		self.password = ''
+        self.dbcon = None
+        self.schemata = []
 
-		self.dbcon = None
-		self.schemata = []
+        QgsMessageLog.logMessage('Initializing Pgdb...', 'Domain Manager')
 
-		QgsMessageLog.logMessage('Initializing Pgdb...', 'Data Manager')
-
-	def connect(self):
-		"""Connect to the database"""
-		conn_string = "dbname='{0}' \
+    def connect(self):
+        """Connect to the database"""
+        conn_string = "dbname='{0}' \
 					host='{1}' \
 					port='{2}' \
 					user='{3}' \
-					password='{4}'".format(self.database, self.host, self.port, self.username, self.password)
+					password='{4}'".format(self.database, self.host, self.port, self.username,
+                            self.password)
 
-		try:
-			self.dbcon = psycopg2.connect(conn_string)
-			QgsMessageLog.logMessage('Connect to database ' + self.database, 'Data Manager')
-		except:
-			print "Unable to connect to database"
-			exit(1)
+        try:
+            self.dbcon = psycopg2.connect(conn_string)
+            QgsMessageLog.logMessage('Connect to database ' + self.database,
+                                     'Domain Manager')
+        except:
+            print("Unable to connect to database")
+            exit(1)
 
-	def check_extension(self):
-		"""Check if extension of information schema views is initialized"""
-		try:
-			cur = self.dbcon.cursor()
-			cur.execute("SELECT COUNT(*) FROM pg_class WHERE relname='foreign_key_constraints_vw'")
-			view_count = cur.fetchone()[0]
-			cur.close()
-			return (view_count == 1)
-		except:
-			return False
+    def check_extension(self):
+        """Check if extension of information schema views is initialized"""
+        try:
+            cur = self.dbcon.cursor()
+            cur.execute(
+                "SELECT COUNT(*) FROM pg_class WHERE relname='foreign_key_constraints_vw'"
+            )
+            view_count = cur.fetchone()[0]
+            cur.close()
+            return (view_count == 1)
+        except:
+            return False
 
-	def init_extension(self):
-		"""Initialize extension of information schema views. Must be connected as superuser"""
-		dir_path = os.path.dirname(os.path.realpath(__file__))
-		sql_file = os.path.join(dir_path, "sql/information_schema_views.sql")
+    def init_extension(self):
+        """Initialize extension of information schema views. Must be connected as superuser"""
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        sql_file = os.path.join(dir_path, "sql/information_schema_views.sql")
 
-		if os.path.exists(sql_file):
-			QgsMessageLog.logMessage("Initializing extension", 'Data Manager')
-			cur = self.dbcon.cursor()
-			with open(sql_file, "r") as sql:
-				code = sql.read()
-				cur.execute(code)
-				self.dbcon.commit()
-			cur.close()
-		else:
-			QgsMessageLog.logMessage("SQL file " + sql_file + " is missing...", 'Data Manager')
-			QgsMessageLog.logMessage("Please reinstall the plugin!", 'Data Manager')
+        if os.path.exists(sql_file):
+            QgsMessageLog.logMessage("Initializing extension", 'Domain Manager')
+            cur = self.dbcon.cursor()
+            with open(sql_file, "r") as sql:
+                code = sql.read()
+                cur.execute(code)
+                self.dbcon.commit()
+            cur.close()
+        else:
+            QgsMessageLog.logMessage("SQL file " + sql_file + " is missing...",
+                                     'Domain Manager')
+            QgsMessageLog.logMessage("Please reinstall the plugin!",
+                                     'Domain Manager')
 
-	def init_schemata(self):
-		"""Init the database schemata and update its own propery list"""
-		cur = self.dbcon.cursor()
+    def init_schemata(self):
+        """Init the database schemata and update its own propery list"""
+        cur = self.dbcon.cursor()
 
-		sql_string = "\
+        sql_string = "\
 			SELECT schema_name \
 			  FROM information_schema.schemata \
              WHERE schema_name <> 'information_schema' \
                AND schema_name NOT LIKE 'pg_%' \
 			 ORDER BY schema_name;"
 
-		cur.execute(sql_string)
-		rows = cur.fetchall()
+        cur.execute(sql_string)
+        rows = cur.fetchall()
 
-		self.schemata = [row[0] for row in rows]
+        self.schemata = [row[0] for row in rows]
 
-		cur.close()
+        cur.close()
 
-	def get_layers(self, schema):
-		"""Get layers list from the database and return to the calling process"""
-		cur = self.dbcon.cursor()
+    def get_layers(self, schema):
+        """Get layers list from the database and return to the calling process"""
+        cur = self.dbcon.cursor()
 
-		sql_string = "\
+        sql_string = "\
 			SELECT info.table_schema, \
 				   info.table_name, \
 				   geom.f_geometry_column, \
@@ -117,38 +122,39 @@ class Pgdb:
 			 WHERE table_schema = '{0}' \
 		  ORDER BY table_schema, table_name;".format(schema)
 
-		cur.execute(sql_string)
-		layers = cur.fetchall()
-		cur.close()
+        cur.execute(sql_string)
+        layers = cur.fetchall()
+        cur.close()
 
-		return(layers)
+        return (layers)
 
-	def get_joined_tables(self, schema, layer):
-		"""Get joined tables list from the database and return to the calling process"""
-		cur = self.dbcon.cursor()
+    def get_joined_tables(self, schema, layer):
+        """Get joined tables list from the database and return to the calling process"""
+        cur = self.dbcon.cursor()
 
-		sql_joins = "\
+        sql_joins = "\
 			SELECT * \
 			  FROM foreign_key_constraints_vw \
 			 WHERE table_schema = '{0}' and table_name = '{1}';".format(schema, layer)
 
-		cur.execute(sql_joins)
-		rows = cur.fetchall()
-		cur.close()
+        cur.execute(sql_joins)
+        rows = cur.fetchall()
+        cur.close()
 
-		return(rows)
+        return (rows)
 
-	def get_related_tables(self, schema, layer):
-		"""Get related tables list from the database and return to the calling process"""
-		cur = self.dbcon.cursor()
+    def get_related_tables(self, schema, layer):
+        """Get related tables list from the database and return to the calling process"""
+        cur = self.dbcon.cursor()
 
-		sql_relations = "\
+        sql_relations = "\
 			SELECT * \
 			  FROM foreign_key_constraints_vw \
-			 WHERE f_table_schema = '{0}' and f_table_name = '{1}';".format(schema, layer)
+			 WHERE f_table_schema = '{0}' and f_table_name = '{1}';".format(
+            schema, layer)
 
-		cur.execute(sql_relations)
-		rows = cur.fetchall()
-		cur.close()
+        cur.execute(sql_relations)
+        rows = cur.fetchall()
+        cur.close()
 
-		return(rows)
+        return (rows)
